7c989494bcda4330ff0902003e6d967b0fac5f5e
Renan Rocha <renanro919@gmail.com>
2022-07-26 09:43:06 -0400

    WPF

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace System_Cont.Properties
+namespace System_Cont_Att.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace System_Cont.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System_Cont.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System_Cont_Att.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
