6701f63470e93f05478c29e496655dc3969b006e
rafaelFelipe <rafaguiar2005@gmail.com>
2022-09-20 08:59:35 -0400

    Conexão finalizada

--- Views/ligacaoPythonTest.xaml.cs
+++ Views/ligacaoPythonTest.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -25,52 +27,31 @@ namespace System_Cont.Views
         {
             InitializeComponent();
         }
-        static void testePython()
-        {
-            var engine = Python.CreateEngine();
+        /*  
+         *              ScriptEngine engine = Python.CreateEngine();
+            engine.ExecuteFile(@"D:\IFRO\Projeto Cont\python\main.py");
+         *  
+                    var engine = Python.CreateEngine();
 
             var script = @"D:\IFRO\Projeto Cont\python\main.py";
             var source = engine.CreateScriptSourceFromFile(script);
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // 1) Create engine
-            var engine = Python.CreateEngine();
-
-            // 2) Provide script and arguments
-            var script = @"C:\Users\rafag\Music\RunPythonScriptFromCS\DaysBetweenDates.py";
-            var source = engine.CreateScriptSourceFromFile(script);
-
-            var argv = new List<string>();
-            argv.Add("");
-            argv.Add("2019-1-1");
-            argv.Add("2019-1-22");
-
-            engine.GetSysModule().SetVariable("argv", argv);
-
-            // 3) Output redirect
-            var eIO = engine.Runtime.IO;
-
-            var errors = new MemoryStream();
-            eIO.SetErrorOutput(errors, Encoding.Default);
-
-            var results = new MemoryStream();
-            eIO.SetOutput(results, Encoding.Default);
-
-            // 4) Execute script
             var scope = engine.CreateScope();
             source.Execute(scope);
 
-            // 5) Display output
-            string str(byte[] x) => Encoding.Default.GetString(x);
+        */
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
 
-            tbxReceba.Text = Convert.ToString(argv[0]);
-            Console.WriteLine("ERRORS:");
-            Console.WriteLine(str(errors.ToArray()));
-            Console.WriteLine();
-            Console.WriteLine("Results:");
-            Console.WriteLine(str(results.ToArray()));
+
+            var p = new Process
+            {
+                StartInfo =
+                 {
+                     FileName = "main.py",
+                     WorkingDirectory = @"D:\IFRO\Projeto Cont\python\",
+                 }
+            }.Start();
         }
     }
 }
