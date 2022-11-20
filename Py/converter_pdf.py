17e67c7af4f12a40c1a64199dc83805576336aa4
rafaelFelipe <rafaguiar2005@gmail.com>
2022-11-20 01:59:58 -0400

    Converter pdf em image
    feito

---
+++ Py/converter_pdf.py
@@ -0,0 +1,16 @@
+import os
+from pdf2image import convert_from_path
+
+
+diretorio_entrada ='C:\\Users\\rafag\\OneDrive\\Documentos\\PDS_projeto\\System-Cont\\Files\\Change\\'
+diretorio_saida ='C:\\Users\\rafag\\OneDrive\\Documentos\\PDS_projeto\\System-Cont\\Files\\Finished\\'
+
+dirs = os.listdir(diretorio_entrada)
+
+for file in dirs:
+    print(diretorio_entrada+file)
+    images = convert_from_path(diretorio_entrada+file)
+
+    for image in images:
+        image.save(diretorio_saida+"%s-pagina-%d.jpg" % (file, images.index(image)+1), "JPEG")
+
